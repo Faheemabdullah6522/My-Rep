af89fad1a68e82a0f8b52bb80ff9a7266b0f8c99
faheem abdullah <chandsajid6522@gmail.com>
2024-04-03 08:50:25 +0500

    my first commit

---
+++ ....py
@@ -0,0 +1,8 @@
+def print_multiplication_table(number):
+    print(f"Multiplication table for {number}:")
+    for i in range(1, 101):
+        print(f"{number} x {i} = {number * i}")
+
+if __name__ == "__main__":
+    user_number = int(input("Enter a number: "))
+    print_multiplication_table(user_number)
